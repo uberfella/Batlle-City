bcf4a4edd36c17c29107bbddd95187e466df863b
Nikita Ivanov <53362543+uberfella@users.noreply.github.com>
2025-06-29 14:56:54 +0700

    main menu continue

---
+++ Assets/ContinueGameButton.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class ContinueGameButton : MonoBehaviour
+{
+    public GameObject continueButton;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (/*PlayerPrefs.HasKey("HasSaveData")*/true)
+        {
+            continueButton.SetActive(true);
+        }
+        else
+        {
+            continueButton.SetActive(false);
+        }
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
