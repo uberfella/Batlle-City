8286ecd803e2ce2e0556e3cb4795264f6ca4cbfb
Nikita Ivanov <53362543+uberfella@users.noreply.github.com>
2026-02-03 13:48:21 +0700

    powerup grid set lew jin jin

--- Assets/My Scripts/PowerupLogic.cs
+++ Assets/My Scripts/PowerupLogic.cs
@@ -15,6 +15,8 @@ public class PowerupLogic : MonoBehaviour
 {
     public GameObject [] powerupsToSpawn;
     //temporary way to make sure the powerup spawns inside squares nicely
+    //-5.5 -6.5 
+    //6.5 5.5
     float[] predefinedPosX = { -5.5f, -4.5f, -3.5f, -2.5f, -1.5f, -0.5f, 0.5f, 1.5f, 2.5f, 3.5f, 4.5f, 5.5f, 6.5f }; //13 entries
     float[] predefinedPosY = { -6.5f, -5.5f, -4.5f, -3.5f, -2.5f, -1.5f, -0.5f, 0.5f, 1.5f, 2.5f, 3.5f, 4.5f, 5.5f };
 
@@ -31,31 +33,25 @@ public class PowerupLogic : MonoBehaviour
             GameObject instance = Instantiate(powerupsToSpawn[3], new Vector2(-1.75f, -5.95f), Quaternion.identity);
             instance.SetActive(true);
         }
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            SpawnRandomPowerupOnField();
-        } 
     }
 
-    //-5.5 -6.5 
-    //6.5 5.5
+
     public void SpawnRandomPowerupOnField()
     {
-        float randomPosX = predefinedPosX[Random.Range(0, 12)];
-        float randomPosY = predefinedPosY[Random.Range(0, 12)];
+        float randomPosX = predefinedPosX[Random.Range(0, 13)];
+        float randomPosY = predefinedPosY[Random.Range(0, 13)];
         int randomPowerUp = Random.Range(0, 6);
         //make sure the powerup doesn't spawn inside the base
-        if((randomPosX == -0.5f & randomPosY == -6.5) || (randomPosX == 0.5f & randomPosY == -6.5f) || (randomPosX == 1.5f & randomPosY == -6.5f) || 
-            (randomPosX == 0.5f & randomPosY == -5.5f) || (randomPosX == 0.5f & randomPosY == -5.5f) || (randomPosX == 1.5f & randomPosY == -5.5f))
+        if (((randomPosY == -6.5f || randomPosY == -5.5f) & (randomPosX == -0.5f || randomPosX == 0.5f || randomPosX == 1.5f)))
         {
-            randomPosX = predefinedPosX[Random.Range(0, 12)];
-            randomPosY = predefinedPosY[Random.Range(2, 12)];
+            randomPosX = predefinedPosX[Random.Range(0, 13)];
+            randomPosY = predefinedPosY[Random.Range(2, 13)];
         }
 
         GameObject instance = Instantiate(powerupsToSpawn[randomPowerUp], new Vector2(randomPosX, randomPosY), Quaternion.identity);
         instance.SetActive(true);
 
-        Destroy(instance, 1000f);
+        Destroy(instance, 10f);
 
     }
 
