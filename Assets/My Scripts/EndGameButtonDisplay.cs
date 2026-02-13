c85cf1e1531c49e22c56cc643249e49ec78284cf
Nikita Ivanov <53362543+uberfella@users.noreply.github.com>
2026-02-13 13:59:58 +0700

    endscreen 9

--- Assets/My Scripts/EndGameButtonDisplay.cs
+++ Assets/My Scripts/EndGameButtonDisplay.cs
@@ -5,15 +5,18 @@ public class EndGameButtonDisplay : MonoBehaviour
 {
     //public static int currentScore = 0;
     //public static int highScore = 0;
-    //hiscore beaten - 
-    //hiscore not beaten - 
+    //hiscore beaten - Game Over ImageText hide in N seconds 
+    //hiscore not beaten - Game Over ImageText not hide, hide static_ScoreHigh and ScoreHighVal, show ButtonsRow and iconSelector in N seconds
     public GameObject buttonsRow;
     public GameObject iconSelector;
+    //public GameObject gameOverImageText;
+    public GameObject static_ScoreHigh;
+    public GameObject scoreHighVal;
 
     void Start()
     {
 
-
+        //static_ScoreHigh.SetActive(true);
         StartCoroutine(ShowHomeButton());
     }
 
