a8eb5a2de91a013c0ad6e603f6499cc115212648
Nikita Ivanov <53362543+uberfella@users.noreply.github.com>
2026-02-26 08:55:27 +0700

    pauseSequence 2

---
+++ Assets/PauseButton.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PauseButton : MonoBehaviour
+{
+    [SerializeField] private RectTransform pauseText;
+    [SerializeField] private Vector2 pausedPosition;
+    [SerializeField] private Vector2 hiddenPosition;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (PauseManager.Instance.isPaused)
+            {
+                PauseManager.Instance.Resume();
+                pauseText.anchoredPosition = pausedPosition;
+            }
+            else
+            {
+                PauseManager.Instance.Pause();
+                pauseText.anchoredPosition = hiddenPosition;
+            }
+        }
+    }
+}
